6dd74096074f4c3ad1069e7c78d1b47aa49f0478
danish0313 <danishoo.dk@gmail.com>
2026-02-21 16:31:31 +0100

    Add gh-pages deployment scripts

--- book/vite.config.ts
+++ book/vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
 
 
 // https://vite.dev/config/
